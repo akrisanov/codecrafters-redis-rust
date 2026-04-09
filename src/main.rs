318107cf32046605ddc02a1b7d3875d312553608
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2026-04-10 00:11:36 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread,
 };
 
+const DEFAULT_IP: &str = "127.0.0.1";
+const DEFAULT_PORT: u32 = 6379;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0u8; 1024];
+
+    loop {
+        match stream.read(&mut buf) {
+            Ok(0) => {
+                break;
+            }
+            Ok(_n) => {
+                if let Err(err) = stream.write_all(b"+PONG\r\n") {
+                    eprint!("write error: {err}");
+                    break;
+                }
+            }
+            Err(err) => {
+                eprint!("read error: {err}");
+                break;
+            }
+        }
+    }
+}
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let addr = format!("{DEFAULT_IP}:{DEFAULT_PORT}");
+    let listener = TcpListener::bind(addr).expect("failed to bind to 127.0.0.0");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buf = [0u8; 1024];
-
-                loop {
-                    let bytes_read = stream.read(&mut buf).unwrap();
-                    if bytes_read == 0 {
-                        break;
-                    }
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
